d0493d6f632a6ee39177b8b1682d89e0bfe620a8
giladyavneh <67857522+giladyavneh@users.noreply.github.com>
2020-11-03 19:31:47 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import IntuitiveTile from './reusables/IntuitiveTile';
+import Map from "./charts/Map"
+import dotenv from "dotenv"
+dotenv.config()
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IntuitiveTile color="teal" tileName="Tile">
+        <Map height={800} width={400} events={[{
+      "_id": "4-OSZ9CUUQy",
+      "session_id": "7bea9e9c-7022-4b38-b36f-43b316ac361a",
+      "name": "signup",
+      "distinct_user_id": "n9i5RjJ8a",
+      "date": 1601990113053,
+      "os": "android",
+      "browser": "safari",
+      "geolocation": {
+        "location": {
+          "lat": -60,
+          "lng": 103
+        },
+        "accuracy": 1446
+      },
+      "url": "http://localhost3000/signup"
+    },]}/>
+      </IntuitiveTile>
     </div>
   );
 }
